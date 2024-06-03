90fe89467d3835fdcefa4f4c23082547fbde7ee4
Aesir64 <serranoneil23@gmail.com>
2024-06-03 22:44:10 +0800

    Add files via upload

---
+++ new menu'/scripts.js
@@ -0,0 +1,25 @@
+var mybutton = document.getElementById("scrollToTopBtn");
+
+window.onscroll = function() {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.documentElement.scrollTop = 0;
+}
+
+function scrollToSection(sectionId) {
+  var section = document.getElementById(sectionId);
+  if (section) {
+    section.scrollIntoView({ behavior: 'smooth' });
+  }
+}
+
